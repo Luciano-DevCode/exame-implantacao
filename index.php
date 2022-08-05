933e13b07247e4e351e1794e6ca622f083270cab
Luciano-DevCode <109243399+Luciano-DevCode@users.noreply.github.com>
2022-08-05 13:05:59 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
 <?php
 
-echo "Parabéns, você criou um site do Azure usando a CLI!";
-echo "\n\nPrint essa tela inteira e cole no classroon!";
+echo nl2br("Parabéns, você criou um site do Azure usando a CLI! \n \n Print essa tela inteira e cole no classroon!");
